75f080ecfdc0fc3eab845fa5d0a49d8297587cf8
B6Infinity <broteendas.official@gmail.com>
2026-05-07 14:42:51 +0530

    change: load geoJSON data from backend server instead of local file

--- lab/road_segment_id/script.js
+++ lab/road_segment_id/script.js
@@ -11,70 +11,85 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Global Vars
 let AKM_Boundary = null;
 let AKM_Roads = null;
+let AKM_Wards = null;
 
 // Map Layer Functions
 function fetchAKMBoundary() {
     // Loading geoJSON data
-    return fetch('AKM_Boundary.geojson')
+    return fetch('http://localhost:4000/api/map/boundary')
         .then(response => response.json())
         .then(data => {
-            AKM_Boundary = data;
+            AKM_Boundary = data.data;
             console.log('AKM_Boundary loaded', AKM_Boundary);
         })
         .catch(error => console.error('Failed to load AKM_Wards.json:', error));
 }
 
 function fetchAKMRoads() {
-    return fetch('AKM_Roads.geojson')
+    return fetch('http://localhost:4000/api/map/roads')
         .then(response => response.json())
         .then(data => {
-            AKM_Roads = data;
+            AKM_Roads = data.data;
             console.log('AKM_Roads loaded:', AKM_Roads.features.length);
         });
 }
 
-function inferAuthority(properties) {
+function fetchAKMWards() {
+    return fetch('http://localhost:4000/api/map/wards')
+        .then(response => response.json())
+        .then(data => {
+            AKM_Wards = data.data;
 
-  if (properties.ref?.startsWith("NH")) {
-    return "NHAI";
-  }
-
-  if (properties.highway === "trunk") {
-    return "PWD";
-  }
-
-  return "Municipality";
+            console.log('AKM_Wards loaded:', AKM_Wards.data);
+        });
 }
 
-async function drawAKMBoundary() {
+// Helpers
+function inferAuthority(properties) {
+
+    if (properties.ref?.startsWith("NH")) {
+        return "NHAI";
+    }
+
+    if (properties.highway === "trunk") {
+        return "PWD";
+    }
+
+    return "Municipality";
+}
+
+async function drawGeoSpatialLayers() {
+    // Fetch em all
     await fetchAKMBoundary();
     await fetchAKMRoads();
+    await fetchAKMWards();
 
-    // Draw the boundaries
+    // Draw em all
+
     // L.geoJSON(AKM_Boundary).addTo(map);
-    // Draw the roads
+
     L.geoJSON(AKM_Roads, {
         onEachFeature: (feature, layer) => {
             const tooltipContent = `OSM ID: ${feature.properties.osm_id || 'N/A'}<br>Name: ${feature.properties.name || 'N/A'}<br>Ref: ${feature.properties.ref || 'N/A'}<br>Authority: ${inferAuthority(feature.properties)}`;
-            layer.bindTooltip(tooltipContent, {direction: 'top', sticky: true});
+            layer.bindTooltip(tooltipContent, { direction: 'top', sticky: true });
 
             layer.on("click", () => {
 
                 console.log("OSM ID:", feature.properties.osm_id);
                 console.log("Name:", feature.properties.name);
                 console.log("Ref:", feature.properties.ref);
-                
+
                 // Evaluate authority (basic MVP prototype)
                 console.log("Authority:", inferAuthority(feature.properties));
-                
+
 
 
             });
         }
     }).addTo(map);
 
+    L.geoJSON(AKM_Wards).addTo(map);
 
-    console.log('Drawn bound');
 }
 
 
@@ -96,4 +111,4 @@ var popup = L.popup();
 
 
 // Execute ----------------------------------
-drawAKMBoundary();
+drawGeoSpatialLayers();
